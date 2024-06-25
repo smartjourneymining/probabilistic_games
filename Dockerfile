smartjourneymining/probabilistic_games

commit 5d7540d5ad08227ce7f84cdc1e73514546c8cd73

Add case study data to container

Dockerfile
@@ -3,6 +3,7 @@ FROM --platform=linux/amd64  debian:bookworm
 
 WORKDIR /home
 COPY requirements.txt .project-root ./
+COPY data ./
 
 RUN <<EOF
     apt-get update
@@ -24,6 +25,9 @@ RUN <<EOF
     pip3 install pygraphviz --break-system-packages
     # install required python libraries
     pip3 install --no-cache-dir -r requirements.txt --break-system-packages
+    # download case studies data
+    curl "https://zenodo.org/records/6962413/files/data.csv?download=1" -o data/data.csv
+    curl "https://data.4tu.nl/articles/dataset/BPI_Challenge_2017/12696884" -o "data/BPI Challenge 2017.xes"
 EOF
 
 COPY probabilistic_game_utils.py \
@@ -35,6 +39,5 @@ COPY probabilistic_game_utils.py \
 # copy project subdirectories
 COPY queries /home/queries
 COPY journepy /home/journepy
-COPY data /home/data
 
 ENTRYPOINT ["python3", "-u", "./run.py"]

commit 007b34a5292f1d1ac3ab661c29c4b1f908d10ff4

Fix docker build failing
Fixes error introduced in commit 5d7540d

Dockerfile
@@ -3,7 +3,7 @@ FROM --platform=linux/amd64  debian:bookworm
 
 WORKDIR /home
 COPY requirements.txt .project-root ./
-COPY data ./
+COPY data data/
 
 RUN <<EOF
     apt-get update